1cb85fee37b69cd261a180735402e069cdcff362
cabe2104 <143588426+cabe2104@users.noreply.github.com>
2024-05-06 15:07:25 +0200

    test för att optimera kod

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 # Authors: Caroline Berglin and Julia Ellström
 # Course: DT099G, Examensarbete
 # Date: 2024-05-02
-
+import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
 
@@ -18,6 +19,12 @@ def train_model(model_type, x_train, y_train):
     """
     if model_type == 'DT':
         model = DecisionTreeClassifier()
+        model.fit(x_train, y_train)
+        while True:
+            weakest_link = np.argmin(model.tree_.impurity)
+            if weakest_link == 0:
+                break
+            model.tree_.children_left[weakest_link] = model.tree_.children_right[weakest_link] = -1
     elif model_type == 'SVM':
         model = svm.SVC(kernel='rbf')
     else:
