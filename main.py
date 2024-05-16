d81e678e07f0d58357e143849d8e8ba86f5f19dd
cabe2104 <143588426+cabe2104@users.noreply.github.com>
2024-05-16 13:30:47 +0200

    Implementerat funktionalitet för att ta ut medelväde och standardavvikelse

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# Författare: Caroline Berglin och Julia Ellström
+# Författare: Caroline Berglin
 # Datum: 2024-04-09
 # Kurs: DT099G, examensarbete
 
 from sklearn.model_selection import train_test_split
-from matplotlib import pyplot as plt
 import decisionTree
 import evaluation
-import SVM
+
 
 def process_dataset(dataset_name, model, test_size):
     """
@@ -28,27 +27,6 @@ def main():
     process_dataset('coverType', decisionTree, 0.3)
     process_dataset('mnist', decisionTree, 10000)
 
-    X, y, class_names = SVM.load_dataset('iris')
 
-    # Split the dataset.
-    X_train, X_test, y_train, y_test = SVM.split_dataset(X, y, 0.3, 17)
-
-    # Train the SVM model.
-    svm_model = SVM.train_svm_model(X_train, y_train)
-
-    # Make predictions.
-    y_pred = SVM.predict_svm_model(svm_model, X_test)
-
-    # Evaluate the model.
-    accuracy, precision, recall, f1 = SVM.evaluate_model(y_test, y_pred)
-
-    # Print the results.
-    print(f"Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-Score: {f1}")
-
-    # Plot the confusion matrix.
-    SVM.plot_confusion_matrix(y_test, y_pred, class_names=class_names)
-    plt.show()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
